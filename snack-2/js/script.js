b5456bd114fd7b87756042af0216bfeb088818c3
MatthiasCominelli <matticomi12@gmail.com>
2022-10-12 16:03:01 +0200

    Snack 2 completato

---
+++ snack-2/js/script.js
@@ -0,0 +1,15 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista
+// di nomi e una lista di cognomi, Gatsby vuole generare una
+// falsa lista di invitati.
+
+
+listaNomi = ["andrea", "francesco", "luca", "marcello", "davide"];
+listaCognomi = ["pacifico", "borriello", "majorano", "vernieri", "capuozzo"];
+falsaLista = [];
+
+
+for (let i = 0; i < 5; i++) {
+    random1 = Math.floor(Math.random() * 5);
+    random2 = Math.floor(Math.random() * 5);
+    document.getElementById('falsaLista').innerHTML += listaNomi[random1] + " " + listaCognomi[random2] + "<br>";
+  }
